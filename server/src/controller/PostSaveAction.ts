0f97d05a9f8d57dc9ffd0255da934dc2b2186b9f
Lisa-Marie <lisamarie@madx.nl>
2020-04-30 20:20:07 +0200

    Got it working again

--- server/src/controller/PostSaveAction.ts
+++ server/src/controller/PostSaveAction.ts
@@ -10,15 +10,13 @@ export async function postSaveAction(request: Request, response: Response) {
 
     // get a post repository to perform operations with post
     const postRepository = getRepository(Post);
-    const categoryRepository = getRepository(Category);
 
     const category = request.body.categories
     category.name = request.body.categories.name
-    await categoryRepository.save(category)
 
     // create a real post object from post json object sent over http
     const post = request.body
-    post.categories = [category]
+    post.categories = category
     const newPost = postRepository.create(post);
 
     // save received post
